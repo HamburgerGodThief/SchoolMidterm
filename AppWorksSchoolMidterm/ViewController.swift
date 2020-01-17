6b1ad6969660a0d927cc473224a33b911bfe1173
Hamburger <chrischenghc@gmail.com>
2020-01-17 09:15:18 +0800

    Initial Commit

---
+++ AppWorksSchoolMidterm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppWorksSchoolMidterm
+//
+//  Created by Hamburger on 2020/1/17.
+//  Copyright © 2020 Hamburger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
